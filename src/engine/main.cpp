5e0bfe66b6552b3a815347f397f7ad5619cc706e
lsalzman <lsalzman@gmail.com>
2012-04-17 15:44:34 +0300

    placeholder logo and linux 32 bins

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -220,12 +220,22 @@ void renderbackground(const char *caption, Texture *mapshot, const char *mapname
         glEnd();
         float lh = 0.5f*min(w, h), lw = lh*2,
               lx = 0.5f*(w - lw), ly = 0.5f*(h*0.5f - lh);
-        settexture((maxtexsize ? min(maxtexsize, hwtexsize) : hwtexsize) >= 1024 && (screen->w > 1280 || screen->h > 800) ? "data/logo_1024.png" : "data/logo.png", 3);
+        settexture(/*(maxtexsize ? min(maxtexsize, hwtexsize) : hwtexsize) >= 1024 && (screen->w > 1280 || screen->h > 800) ? "data/logo_1024.png" :*/ "data/logo.png", 3);
         glBegin(GL_TRIANGLE_STRIP);
         glTexCoord2f(0, 0); glVertex2f(lx,    ly);
         glTexCoord2f(1, 0); glVertex2f(lx+lw, ly);
         glTexCoord2f(0, 1); glVertex2f(lx,    ly+lh);
         glTexCoord2f(1, 1); glVertex2f(lx+lw, ly+lh);
+        glEnd();
+
+        float bh = 0.1f*min(w, h), bw = bh*2,
+              bx = w - 1.25f*bw, by = 0.25f*bh;
+        settexture("data/cube2badge.png", 3);
+        glBegin(GL_TRIANGLE_STRIP);
+        glTexCoord2f(0, 0); glVertex2f(bx,    by);
+        glTexCoord2f(1, 0); glVertex2f(bx+bw, by);
+        glTexCoord2f(0, 1); glVertex2f(bx,    by+bh);
+        glTexCoord2f(1, 1); glVertex2f(bx+bw, by+bh);
         glEnd();
 
         if(caption)
