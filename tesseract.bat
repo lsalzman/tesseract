4d03d76458bd433950e5e6d1b44492c2fb86e865
lsalzman <lsalzman@gmail.com>
2012-04-17 15:27:52 +0300

    add some default -k options to bat files

--- tesseract.bat
+++ tesseract.bat
@@ -1,1 +1,1 @@
-start bin\sauerbraten.exe "-q$HOME\My Games\Tesseract" -ktesseract -glog.txt %*
+start bin\tesseract.exe "-q$HOME\My Games\Tesseract" -k..\Sauerbraten\packages -ktesseract -glog.txt %*
