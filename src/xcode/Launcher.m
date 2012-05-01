1db7ecc600492c0d0de78bb2adc408499f908639
fernLightning <rpointon@fernlightning.com>
2012-05-01 18:56:44 +1200

    osx - fix fullscreen in launcher

--- src/xcode/Launcher.m
+++ src/xcode/Launcher.m
@@ -508,8 +508,7 @@ static int numberForKey(CFDictionaryRef desc, CFStringRef key)
     [args addObject:[NSString stringWithFormat:@"-w%@", [res objectAtIndex:0]]];
     [args addObject:[NSString stringWithFormat:@"-h%@", [res objectAtIndex:1]]];
     [args addObject:@"-z24"]; // otherwise seems to have a fondness to use -z16
-	
-    if ([defs integerForKey:dkFULLSCREEN] == 0) [args addObject:@"-t"];
+    [args addObject:[NSString stringWithFormat:@"-t%d", [defs integerForKey:dkFULLSCREEN]]];
     [args addObject:[NSString stringWithFormat:@"-a%d", [defs integerForKey:dkFSAA]]];
     [args addObject:[NSString stringWithFormat:@"-f%d", [defs integerForKey:dkSHADER]]];
     
