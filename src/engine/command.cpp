f1739cb8dc873a01781c0fba8ac7facad07116b9
lsalzman <lsalzman@gmail.com>
2012-06-01 20:10:50 -0700

    more loopconc fixes

--- src/engine/command.cpp
+++ src/engine/command.cpp
@@ -2167,7 +2167,7 @@ char *loopconc(ident *id, int n, uint *body, bool space)
 {
     identstack stack;
     vector<char> s;
-    loopi(*n)
+    loopi(n)
     {
         setiter(*id, i, stack);
         tagval v;
