8491e61705726c295fe3589a78d1dc4d41aab2af
Lee Salzman <lsalzman@gmail.com>
2012-08-25 22:32:07 +0300

    bat cleanups

--- server.bat
+++ server.bat
@@ -1,1 +1,12 @@
-start bin\tesseract.exe "-q$HOME\My Games\Tesseract" -ktesseract -gserver-log.txt -d %*
+@ECHO OFF
+
+set TESS_BIN=bin
+
+IF /I "%PROCESSOR_ARCHITECTURE%" == "amd64" (
+    set TESS_BIN=bin64
+)
+IF /I "%PROCESSOR_ARCHITEW6432%" == "amd64" (
+    set TESS_BIN=bin64
+)
+
+start %TESS_BIN%\tesseract.exe "-q$HOME\My Games\Tesseract" -ktesseract -gserver-log.txt -d %*
