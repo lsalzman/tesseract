9e3798fd1629ef27c8cc1d815fbeab4570eb8770
lsalzman <lsalzman@gmail.com>
2013-04-02 14:30:14 +0300

    remove obsolete cross compiler script

--- src/cross.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#make CXX=i586-mingw32msvc-g++ STRIP=i586-mingw32msvc-strip WINDRES=i586-mingw32msvc-windres PLATFORM=MINGW $*
-make CXX=i686-w64-mingw32-g++ STRIP=i686-w64-mingw32-strip WINDRES=i686-w64-mingw32-windres PLATFORM=MINGW $*
-
