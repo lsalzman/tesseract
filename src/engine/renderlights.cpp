0feef0fe5ece48c50f2962d0e1cabf746da5ef40
lsalzman <lsalzman@gmail.com>
2013-04-17 13:01:45 +0300

    smaller rsm spread

--- src/engine/renderlights.cpp
+++ src/engine/renderlights.cpp
@@ -1392,7 +1392,7 @@ VARF(rsmprec, 0, 0, 3, cleanupradiancehints());
 FVAR(rhnudge, 0, 0.5f, 4);
 FVARF(rhsplitweight, 0.20f, 0.6f, 0.95f, clearradiancehintscache());
 VARF(rhgrid, 3, 27, 128, cleanupradiancehints());
-FVARF(rsmspread, 0, 0.2f, 1, clearradiancehintscache());
+FVARF(rsmspread, 0, 0.15f, 1, clearradiancehintscache());
 FVARF(rhaoheight, 0, 0.1f, 1, clearradiancehintscache());
 VAR(rhclipgrid, 0, 1, 1);
 VARF(rhcache, 0, 1, 1, cleanupradiancehints());
