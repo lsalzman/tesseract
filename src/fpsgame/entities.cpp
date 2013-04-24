5c065dee4163b1093d6358c2de712cd2942bb0ea
lsalzman <lsalzman@gmail.com>
2013-04-24 13:42:47 +0300

    remove some old ent version handling

--- src/fpsgame/entities.cpp
+++ src/fpsgame/entities.cpp
@@ -12,13 +12,6 @@ namespace entities
 
     void readent(entity &e, char *buf, int ver)     // read from disk, and init
     {
-        if(ver <= 30) switch(e.type)
-        {
-            case FLAG:
-            case TELEDEST:
-                e.attr1 = (int(e.attr1)+180)%360;
-                break;
-        }
     }
 
 #ifndef STANDALONE
